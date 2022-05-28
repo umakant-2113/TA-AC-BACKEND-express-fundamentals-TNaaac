d97eefd56cfa6a5180219351770dceaabfdb4598
umakant-2113 <lodhiumakant800@gmail.com>
2022-05-28 16:13:20 +0530

    Done

--- block-BNaabq/sample/index.js
+++ block-BNaabq/sample/index.js
@@ -1,29 +1,25 @@
 var express=require('express');
 var logger=require('morgan');
 var morgan = require('morgan');
-
 var cookieparser=require('cookie-parser')
-
 var app=express();
-
 app.use(cookieparser())
-
 app.use(logger('dev'))
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(__dirname+"/public"))
+
 app.use((req,res,next)=>{
-res.cookie("username","umakant")
-next()
-})
-app.use((req,res,next)=>{
-res.cookie("user","rajput")
-next()
-});
-app.use((req,res,next)=>{
-res.cookie("class","middleware")
-next()
+    console.log(req.cookies)
 })
 
-app.get("/about",(req,res)=>{
+app.use('/about',(req,res,next)=>{
+res.cookie("username","arya")
+res.end(req.cookies);
+})
+
+app.get("/",(req,res)=>{
     res.send('welcome to express')
 })
 
